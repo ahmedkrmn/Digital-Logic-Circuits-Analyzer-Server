efa2f9012be4051d28ffc2451901f6ede1e936cc
AhmedElgamiel <1500078@eng.asu.edu.eg>
2020-05-11 00:12:18 +0200

    agein

--- controllers/final project/NOT.py
+++
@@ -1,9 +0,0 @@
-from gate import gate
-
-class NOT (gate) :
-
-    def calc(self):
-        if self.input1 == 1 :
-            self.output = 0
-        elif self.input1 == 0:
-            self.output = 1
