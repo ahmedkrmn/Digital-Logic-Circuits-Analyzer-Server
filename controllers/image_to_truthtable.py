ae64bf91c362dcfe9981213fa87e4c1bd5b63254
Ahmed Karaman <20889958+ahmedkrmn@users.noreply.github.com>
2020-05-11 04:20:58 +0200

    Remove debug print statements

--- controllers/image_to_truthtable.py
+++ controllers/image_to_truthtable.py
@@ -35,9 +35,6 @@ temp4 = cv2.imread("/app/controllers/templates/not.PNG", cv2.IMREAD_GRAYSCALE)
 # greimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # template = cv2.imread("messi_face.jpg", 0)
 # w, h = template.shape[::-1]
-print(img)
-print("****************")
-print(temp4)
 # ************************ Gate Detection *********************************
 
 # ******************************* getting height and width for each gate ***************
