58af07d79beef6d2c876ea2b4586989127b19d48
Ahmed Karaman <20889958+ahmedkrmn@users.noreply.github.com>
2020-05-11 05:50:39 +0200

    Update test data

--- controllers/test.py
+++ controllers/test.py
@@ -1,26 +1,6 @@
 # image path = '/app/uploads/image'
 import json
 
-data = [
-  {
-    "x": 0,
-    "y": 0,
-    "z": 0
-  },
-  {
-    "x": 0,
-    "y": 1,
-    "z": 1
-  },
-  {
-    "x": 1,
-    "y": 0,
-    "z": 0
-  },
-    {
-    "x": 1,
-    "y": 1,
-    "z": 1
-  }
-]
+data = [[[0, 0], 0], [[0, 1], 1], [[1, 0], 0], [[1, 1], 1]]
+
 print(json.dumps(data))
